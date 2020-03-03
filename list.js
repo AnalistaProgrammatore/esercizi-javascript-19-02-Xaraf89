7da127e345b40afe6b71732f685072f020283d1f
Xaraf89 <59976249+Xaraf89@users.noreply.github.com>
2020-03-03 23:15:24 +0100

    Update list.js

--- list.js
+++ list.js
@@ -1,3 +1,9 @@
 /**
 * Scrivere qui il codice dell'esercizio 3 del capitolo 4 di eloquentJS
 **/
+var ripeti , numeri = [];
+while(ripeti !== "n"){
+    numeri.push(prompt("aggiungi il numero all'array"));
+    ripeti = prompt('vuoi continuare a scrivere numeri y/n');
+    console.log(numeri + " <br />");
+}
