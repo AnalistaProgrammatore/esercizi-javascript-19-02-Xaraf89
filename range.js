28e6a7103e162bf78c40658f5c94276ae50d4fc4
Xaraf89 <59976249+Xaraf89@users.noreply.github.com>
2020-03-03 23:14:24 +0100

    Update range.js

--- range.js
+++ range.js
@@ -1,3 +1,31 @@
 /**
 * Scrivere qui il codice dell'esercizio 1 del capitolo 4 di eloquentJS
 **/
+var range = function(start, end){
+    var array=[];
+    counter=start;
+    while(counter<end){
+        array.push(counter);
+        counter++;
+    }
+    return array
+}
+var somma = function(array){
+    var totale=0;
+    while(array.lenght>0){
+        totale=totale+array.pop();
+    }
+    return totale
+}
+console.log(somma(range(1, 10)))
+
+/*oppure*/ 
+
+var somma2 = function(array){
+    var totale=0;
+    for(i=0; i<array.lenght ; i++){
+        totale = totale + array[i];
+    }
+    return totale
+}
+console.log(somma2(range(1, 10)))
